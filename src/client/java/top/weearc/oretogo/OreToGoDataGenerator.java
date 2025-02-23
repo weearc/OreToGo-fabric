33f41a9dd122e754e0e38389c1a128a21baed429
weearc <qby19981121@gmail.com>
2025-02-23 18:53:45 +0800

    init

---
+++ src/client/java/top/weearc/oretogo/OreToGoDataGenerator.java
@@ -0,0 +1,11 @@
+package top.weearc.oretogo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OreToGoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
